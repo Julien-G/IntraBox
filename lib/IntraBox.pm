6cb14ffbde56d9dc347406c9be7ea989a753e21d
Julien Girault <julien.emac@gmail.com>
2012-02-20 22:44:24 +0100

    Add stats in the administration

--- lib/IntraBox.pm
+++ lib/IntraBox.pm
@@ -178,6 +178,7 @@ use adminAdminController;
 use adminGroupController;
 use adminSearchController;
 use adminFileController;
+use adminStatsController;
 use purgeController;
 
 #------------------------------------------------------------
